f27c6e5cd2e7e68101e952d8bbdac88d6fa7ec13
kzkonno <kaz_konno@teneocto.io>
2023-08-15 20:36:11 +0900

    update mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://crewzusa0905.github.io',
-  base: 'https://crewzusa0905.github.io/krewztech',
-  integrations: [mdx(), sitemap(), react(), tailwind()]
+  base: '/krewztech',
 });
