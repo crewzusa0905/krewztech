ba219cc265f519ccdbb1e42536fdf592a51e68a5
kzkonno <kaz_konno@teneocto.io>
2023-08-15 21:08:05 +0900

    update mjs base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://crewzusa0905.github.io',
-  base: 'https://github.com/crewzusa0905/krewztech',
+  base: 'https://github.com/crewzusa0905/krewztech/dist',
 });
