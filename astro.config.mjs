2576da250b477f3c9923959737bad118b8a416a2
kzkonno <kaz_konno@teneocto.io>
2023-08-15 20:11:04 +0900

    update mjs file

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://crewzusa0905.github.io',
+  base: '/krewztech',
   integrations: [mdx(), sitemap(), react(), tailwind()]
 });
